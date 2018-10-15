44ddc87bd696766d3e460a706b5fefa3b2bad676
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-10-15 19:14:57 +0200

    Update local, ropsten, rinkeby and kovan network configs

--- solidity/truffle.js
+++ solidity/truffle.js
@@ -6,11 +6,33 @@ global.h = require('./helpers');
 module.exports = {
   network: "test",
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 18545,
-      network_id: "*",
-      gas: 4700000
-    }
+    local: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      gas: 5000000,
+      gasPrice: 2e9, // 2 Gwei
+    },
+    ropsten: {
+      host: 'localhost',
+      port: 8565,
+      network_id: 3,
+      gas: 500000,
+      gasPrice: 10e9, // 10 Gwei
+    },
+    rinkeby: {
+      host: 'localhost',
+      port: 8565,
+      network_id: 4,
+      gas: 500000,
+      gasPrice: 10e9, // 10 Gwei
+    },
+    kovan: {
+      host: 'localhost',
+      port: 8555,
+      network_id: 42,
+      gas: 500000,
+      gasPrice: 10e9, // 10 Gwei
+    },
   }
 };
